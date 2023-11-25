393566d1b57602aa533ac73dcf8d8a1bfc21fe0c
AldoVasquez07 <142806675+AldoVasquez07@users.noreply.github.com>
2023-11-24 21:03:59 -0500

    Creando la Estructura principal Carretera
    Esta estructura, tiene dentro suyo 4 atributos principales: id, ciudad1, ciudad2, costo. La elección de estos parámetros es debido a lo que se solicita en el LAB-09. En el cual instancia que se leerán dichos atributos de un vector de strings. Asimismo, dentro de esta estructura, declaramos su constructor.

--- reconstruyendoCarreteras.cpp
+++ reconstruyendoCarreteras.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 map<string, string> padre;
 
+struct Carretera {
+    string id;
+    string ciudad1;
+    string ciudad2;
+    int costo;
+
+    Carretera(const string& i, const string& c1, const string& c2, int co = -1) : id(i), ciudad1(c1), ciudad2(c2), costo(co) {}
+};
+
 int main() {
     cout<<"Hello World";
 
